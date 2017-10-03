5b53b0325b88105a7e7dc8ca936f056659ddd20a
Caleb Howard <choward@covermymeds.com>
2017-10-02 22:01:17 -0400

    Create User

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Item_type , Item_status, User
+from django.contrib.auth.forms import UserCreationForm
 
 class AddBookForm(forms.Form):
     item_id = forms.IntegerField
@@ -21,3 +22,14 @@ class UpdateBorrowerForm(forms.Form):
   #  def __str__(self):
    #  return User.name
 
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
